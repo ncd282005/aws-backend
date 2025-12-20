382b28b745095deb97721799d783ac5e2a4759c9
siddharthkp401 <siddharthkp401@gmail.com>
2025-12-20 23:27:33 +0530

    fix: remove static client name

--- config/s3Config.js
+++ config/s3Config.js
@@ -17,9 +17,6 @@ const S3_BUCKET_REGION = process.env.AWS_REGION || "ap-south-1";
 // Dedicated error log bucket/key (can reuse main bucket when unspecified)
 const ERROR_LOG_BUCKET_NAME =
   process.env.ERROR_LOG_BUCKET_NAME || "testdevopsetl";
-const ERROR_LOG_OBJECT_KEY =
-  process.env.ERROR_LOG_OBJECT_KEY ||
-  "processeddata/test_sunglasses/logs/error.json";
 
 const PROCESSED_PRODUCTS_BUCKET_NAME =
   process.env.PROCESSED_PRODUCTS_BUCKET_NAME || ERROR_LOG_BUCKET_NAME;
@@ -31,7 +28,6 @@ module.exports = {
   S3_BUCKET_NAME,
   S3_BUCKET_REGION,
   ERROR_LOG_BUCKET_NAME,
-  ERROR_LOG_OBJECT_KEY,
   PROCESSED_PRODUCTS_BUCKET_NAME,
   PROCESSED_PRODUCTS_BASE_PREFIX,
 };
